e4ef079135531b032852367b26006c2e14135ce7
Almaz <hello@akun.dev>
2023-08-09 18:40:01 +0900

    re init

--- jeonse/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from jeonse.models import Listing
-
-class ListingForm(forms.ModelForm):
-    class Meta:
-        model = Listing
-        fields = "__all__"
-        exclude = ["creator"]
--- jeonse/tables.py
+++
@@ -1,9 +0,0 @@
-import django_tables2 as tables
-
-from jeonse.models import Listing
-
-class ListingTable(tables.Table):
-    class Meta:
-        model = Listing
-        template_name = "django_tables2/bootstrap5.html"
-        attrs = {"class": "table table-striped table-bordered table-hover"}
